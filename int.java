9a9c9cec12db28137ae3013ac8486a2ed74c4440
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-08-18 09:56:21 +0530

    Update and rename README.md to int.java

---
+++ int.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num = 5542;
+String number = String.valueOf(num);
+for(int i = 0; i < number.length(); i++) {
+    int j = Character.digit(number.charAt(i), 10);
+    System.out.println("digit: " + j);
+}
+}
+}
